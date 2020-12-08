dcfb348f924ed7fcc20f8af3e4bab47d93fbf0fc
Colin Bendell <colin@bendell.ca>
2020-12-08 17:18:10 -0500

    add glob match and md5

--- src/utils.js
+++ src/utils.js
@@ -7,13 +7,21 @@ const { stringify } = require('./stringify');
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 Promise.delay = sleep;
 
+function md5(data){
+    return crypto.createHash('md5').update(data.toString()).digest('hex');
+}
+
 async function md5File(filename){
-    return new Promise(resolve => {
-        const hash = crypto.createHash('md5');
-        fs.createReadStream(filename)
-            .on('data', data => hash.update(data))
-            .on('end', () => resolve(hash.digest('hex')));
-    });
+    if (typeof filename === 'string' && fs.existsSync(filename)) {
+        return new Promise(resolve => {
+            const hash = crypto.createHash('md5');
+            fs.createReadStream(filename)
+                .on('data', data => hash.update(data))
+                .on('end', () => resolve(hash.digest('hex')));
+        });
+    }
+
+    return crypto.createHash('md5').update(filename.toString()).digest('hex');
 }
 
 async function* getFiles(dir) {
@@ -42,10 +50,46 @@ function isSame(a, b, ignoreProperties=[]) {
     return stringify(left) === stringify(right);
 }
 
+function globAsRegex (glob = "") {
+    if (!glob || typeof glob !== 'string') return;
+
+    // .() => \.\(\)
+    glob = glob.replace(/([/$^+.()=!|])/g, "\\$1");
+    // {foo} => (foo)
+    // {foo,bar} => (foo|bar)
+    for (const match of glob.matchAll(/({[^}]+})/g)) {
+        glob = glob.replace(match[0], "(?:" + match[1].replace(/,/g, "|") + ")");
+    }
+    // foo*bar => foo[^/]*bar
+    // *bar => [^/]*bar
+    // bar* => foo[^/]*
+    // foo**bar => foo.*bar
+    // **bar => .*bar
+    // foo** => foo.*
+    for (const match of glob.matchAll(/([^*]|^)(\*+)([^*]|$)/g)) {
+        if (match[2].length > 1) {
+            glob = glob.replace(match[0], match[1] + ".*" + match[3]);
+        }
+        else {
+            glob = glob.replace(match[0], match[1] + "[^/]*" + match[3]);
+        }
+    }
+    // , => \,
+    glob = glob.replace(/,/g, "\\,");
+
+    // remove the final / since it will be added at the end
+    // foobar/ => foobar
+    glob = glob.replace(/\/$/g, "");
+    // foobar => ^foobar($|/)
+    return new RegExp("^" + glob + "(?:$|/)", "i");
+}
+
 module.exports = {
     cleanObject,
     isSame,
     getFiles,
+    globAsRegex,
+    md5,
     md5File,
     sleep
 };
