b27e4bff50693a6f2eb2cf850567216228db7488
Colin Bendell <colin@bendell.ca>
2021-01-31 19:19:25 -0500

    simplify usage

--- src/cli/shopify.js
+++ src/cli/shopify.js
@@ -8,6 +8,9 @@ program
     .version('1.0');
 
 program
+    .option('--debug', 'enable debug', false)
+    .option('--verbose', 'enable verbose', false)
+    .option('--outputDir <dir>', 'location to save the store files', "./")
     .command('store', 'manage the Shopify Online Store', {isDefault: true})
     .command('products', 'Manage products and inventory');
 
@@ -15,7 +18,7 @@ if (process.argv.indexOf("--debug") === -1) console.debug = function() {};
 if (process.argv.indexOf("--verbose") === -1 && process.argv.indexOf("--debug") === -1) console.info = function() {};
 
 if (process.argv.length <= 2) program.help();
-if (!['store', 'products'].includes(process.argv[2])) process.argv = [].concat(process.argv.slice(0,2), 'store', process.argv.slice(2));
+//if (!['store', 'products'].includes(process.argv[2])) process.argv = [].concat(process.argv.slice(0,2), 'store', process.argv.slice(2));
 
 program
     .parse(process.argv); // end with parse to parse through the input
